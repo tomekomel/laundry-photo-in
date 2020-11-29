1d44450a70bd6e952005a4c9f05666f140bba2c8
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-11-29 10:17:31 +0100

    feat: create gallery module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GalleryModule } from './gallery/gallery.module';
 
 @Module({
-  imports: [],
+  imports: [GalleryModule],
   controllers: [AppController],
   providers: [AppService],
 })
