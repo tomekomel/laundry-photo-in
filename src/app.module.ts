c7cf47060491242b3c0c263c68556b5a2741e599
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-12-01 00:17:36 +0100

    feat: add user module stub

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GalleryModule } from './gallery/gallery.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     GalleryModule,
     TypeOrmModule.forRoot(),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
