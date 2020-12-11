793e61b883b240f51079dde0926bfaf4c8ea7814
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-12-11 13:03:16 +0100

    feat: add profile view, fix in login form

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,9 +4,11 @@ import {
   Post,
   Render,
   Request,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -20,5 +22,10 @@ export class AuthController {
   @Post('login')
   async login(@Request() req) {
     return req.user;
+  }
+
+  @Get('/logout')
+  logout(@Res() res: Response): void {
+    res.redirect('/');
   }
 }
