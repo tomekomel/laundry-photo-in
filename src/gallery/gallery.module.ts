fe64e5a3baa5ae59134ca01c6a6b0a74242bbfa7
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-12-25 13:00:26 +0100

    feat: add photo controller

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { GalleryController } from './controllers/gallery.controller';
 import { Gallery } from './entities/gallery.entity';
 import { GalleryService } from './services/gallery.service';
 import { Country } from './entities/country.entity';
 import { CountryService } from './services/country.service';
+import { PhotoController } from './controllers/photo.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Gallery, Country])],
-  controllers: [GalleryController],
+  imports: [
+    TypeOrmModule.forFeature([Gallery, Country]),
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
+  controllers: [GalleryController, PhotoController],
   providers: [GalleryService, CountryService],
 })
 export class GalleryModule {}
