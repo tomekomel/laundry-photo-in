5ee8e8bca5b222310a64e2e77831ab79a3f60fb5
Tomasz Omeljaniuk <tomekomel@gmail.com>
2021-01-24 13:09:35 +0100

    feat: add controller methods

--- src/favorite/controllers/favorite.controller.ts
+++ src/favorite/controllers/favorite.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { CreateFavoriteDto } from '../dtos/create-favorite.dto';
+import { Response } from 'express';
+import { FavoriteService } from '../services/favorite.service';
 
-@Controller('favorite')
-export class FavoriteController {}
+@Controller('favorites')
+export class FavoriteController {
+  constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Post()
+  async createFavorite(
+    @Body() createFavoriteDto: CreateFavoriteDto,
+    @Res() response: Response,
+  ) {
+    await this.favoriteService.create(createFavoriteDto);
+  }
+
+  @Get()
+  async getFavorites() {
+    return await this.favoriteService.findByUserId(1);
+  }
+}
--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FavoriteController } from './controllers/favorite.controller';
+import { FavoriteService } from './services/favorite.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Favorite } from './entities/favorite.entity';
 
 @Module({
-  controllers: [FavoriteController]
+  imports: [TypeOrmModule.forFeature([Favorite])],
+  controllers: [FavoriteController],
+  providers: [FavoriteService],
 })
 export class FavoriteModule {}
