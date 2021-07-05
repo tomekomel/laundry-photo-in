1e18f914f54d2b65da002a460cd3e76b2b3262e1
Tomasz Omeljaniuk <tomekomel@gmail.com>
2021-07-05 17:18:32 +0200

    Revert "feat: rebuild docker-compose to pgsql"
    This reverts commit 051e46d0ed753b268bf18ab84cf5d22e2c10c388.

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { EmailModule } from './email/email.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
+        type: 'mysql',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
